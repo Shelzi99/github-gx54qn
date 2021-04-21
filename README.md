8a15c2857b8abf2ce4b0095cb8b06ae8fae911ec
Shelzi99 <criv.shelley@gmail.com>
2021-04-21 23:54:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-gx54qn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-gx54qn)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+  title = 'todo-list';
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
+}
+
